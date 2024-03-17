a8a4abccc309f94cdeee90ca3a7b6b26e3cd3b80
nupniichan <bao96065@gmail.com>
2024-03-17 17:30:58 +0700

    update: change conservation engine, delete saucenao API and fix some bugs

--- ChinoKafuBotDiscord/CommandsFolder/PrefixCommandsFolder/BasicCommands.cs
+++ ChinoKafuBotDiscord/CommandsFolder/PrefixCommandsFolder/BasicCommands.cs
@@ -1,13 +1,6 @@
-﻿using DSharpPlus;
-using DSharpPlus.CommandsNext;
+﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
-using DSharpPlus.EventArgs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChinoBot.CommandsFolder.PrefixCommandsFolder
 {
