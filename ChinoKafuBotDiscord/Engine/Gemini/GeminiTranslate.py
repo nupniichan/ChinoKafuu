5f0389955eab95a841eee12ae5f4c4846b2aef3b
nupniichan <bao96065@gmail.com>
2024-06-29 16:56:58 +0700

    Refactor: Change gemini engine from gemini-1.5-pro to gemini-1.5-flash

--- ChinoKafuBotDiscord/Engine/Gemini/GeminiTranslate.py
+++ ChinoKafuBotDiscord/Engine/Gemini/GeminiTranslate.py
@@ -14,7 +14,7 @@ def GeminiTranslate(apiKey, message_content):
       "temperature": 0.9,
       "top_p": 1,
       "top_k": 1,
-      "max_output_tokens": 512,
+      "max_output_tokens": 1024,
     }
 
     safety_settings = [
@@ -36,7 +36,7 @@ def GeminiTranslate(apiKey, message_content):
       },
     ]
 
-    model = genai.GenerativeModel(model_name="gemini-1.5-pro",
+    model = genai.GenerativeModel(model_name="gemini-1.5-flash",
                                   generation_config=generation_config,
                                   safety_settings=safety_settings)
 
