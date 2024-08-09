bfefad52158411cae27d46c3e123287c1858f9f7
nupniichan <bao96065@gmail.com>
2024-08-10 00:05:20 +0700

    feat: Added markdown

--- ChinoKafuBotDiscord/Helper.cs
+++ ChinoKafuBotDiscord/Helper.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ChinoBot
@@ -22,25 +23,25 @@ namespace ChinoBot
         }
         public static string getOsuRank(string rank, string mode, InteractionContext ctx)
         {
-                switch (rank)
-                {
-                    case "A":
-                        return DiscordEmoji.FromName(ctx.Client, ":rankingA:", true);
-                    case "B":
-                        return DiscordEmoji.FromName(ctx.Client, ":rankingB:", true);
-                    case "C":
-                        return DiscordEmoji.FromName(ctx.Client, ":rankingC:", true);
-                    case "D":
-                        return DiscordEmoji.FromName(ctx.Client, ":rankingD:", true);
-                    case "S":
-                        return DiscordEmoji.FromName(ctx.Client, ":rankingS:", true);
-                    case "X":
-                        return DiscordEmoji.FromName(ctx.Client, ":rankingX:", true);
-                    case "SH":
-                        return DiscordEmoji.FromName(ctx.Client, ":rankingSH:", true);
-                    case "XH":
-                        return DiscordEmoji.FromName(ctx.Client, ":rankingXH:", true);
-                }
+            switch (rank)
+            {
+                case "A":
+                    return DiscordEmoji.FromName(ctx.Client, ":rankingA:", true);
+                case "B":
+                    return DiscordEmoji.FromName(ctx.Client, ":rankingB:", true);
+                case "C":
+                    return DiscordEmoji.FromName(ctx.Client, ":rankingC:", true);
+                case "D":
+                    return DiscordEmoji.FromName(ctx.Client, ":rankingD:", true);
+                case "S":
+                    return DiscordEmoji.FromName(ctx.Client, ":rankingS:", true);
+                case "X":
+                    return DiscordEmoji.FromName(ctx.Client, ":rankingX:", true);
+                case "SH":
+                    return DiscordEmoji.FromName(ctx.Client, ":rankingSH:", true);
+                case "XH":
+                    return DiscordEmoji.FromName(ctx.Client, ":rankingXH:", true);
+            }
             return null;
         }
         public static string FormatDate(DateTime? date)
@@ -63,7 +64,6 @@ namespace ChinoBot
                 switch (childNode.NodeType)
                 {
                     case HtmlNodeType.Element:
-                        // Xử lý các thẻ HTML
                         switch (childNode.Name)
                         {
                             case "b":
@@ -86,6 +86,55 @@ namespace ChinoBot
                                 markdownBuilder.Append(ProcessHtmlToMarkdown(childNode));
                                 markdownBuilder.Append("~~");
                                 break;
+                            case "h1":
+                                markdownBuilder.Append("# ");
+                                markdownBuilder.Append(ProcessHtmlToMarkdown(childNode));
+                                markdownBuilder.AppendLine();
+                                break;
+                            case "h2":
+                                markdownBuilder.Append("## ");
+                                markdownBuilder.Append(ProcessHtmlToMarkdown(childNode));
+                                markdownBuilder.AppendLine();
+                                break;
+                            case "ul":
+                            case "ol":
+                                foreach (var listItem in childNode.ChildNodes)
+                                {
+                                    if (listItem.Name == "li")
+                                    {
+                                        markdownBuilder.Append(childNode.Name == "ul" ? "* " : "1. ");
+                                        markdownBuilder.Append(ProcessHtmlToMarkdown(listItem));
+                                        markdownBuilder.AppendLine();
+                                    }
+                                }
+                                break;
+                            case "a":
+                                var href = childNode.GetAttributeValue("href", string.Empty);
+                                markdownBuilder.Append("[");
+                                markdownBuilder.Append(ProcessHtmlToMarkdown(childNode));
+                                markdownBuilder.Append("](");
+                                markdownBuilder.Append(href);
+                                markdownBuilder.Append(")");
+                                break;
+                            case "img":
+                                var src = childNode.GetAttributeValue("src", string.Empty);
+                                var alt = childNode.GetAttributeValue("alt", string.Empty);
+                                markdownBuilder.Append("![");
+                                markdownBuilder.Append(alt);
+                                markdownBuilder.Append("](");
+                                markdownBuilder.Append(src);
+                                markdownBuilder.Append(")");
+                                break;
+                            case "code":
+                                markdownBuilder.Append("`");
+                                markdownBuilder.Append(ProcessHtmlToMarkdown(childNode));
+                                markdownBuilder.Append("`");
+                                break;
+                            case "pre":
+                                markdownBuilder.Append("```\n");
+                                markdownBuilder.Append(ProcessHtmlToMarkdown(childNode));
+                                markdownBuilder.Append("\n```");
+                                break;
                             default:
                                 markdownBuilder.Append(ProcessHtmlToMarkdown(childNode));
                                 break;
@@ -98,6 +147,18 @@ namespace ChinoBot
             }
 
             return markdownBuilder.ToString();
+        }
+
+
+        public static string GetUtcOffsetString()
+        {
+            TimeZoneInfo localTimeZone = TimeZoneInfo.Local;
+            TimeSpan utcOffset = localTimeZone.GetUtcOffset(DateTime.UtcNow);
+
+            string sign = utcOffset >= TimeSpan.Zero ? "+" : "-";
+            string formattedOffset = $"{sign}{Math.Abs(utcOffset.Hours):00}:{Math.Abs(utcOffset.Minutes):00}";
+
+            return $"UTC{formattedOffset}";
         }
     }
 }
