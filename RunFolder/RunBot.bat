11146181a89d0f56168f007a72f13a886c732356
nupniichan <bao96065@gmail.com>
2024-08-27 17:15:52 +0700

    chore: change execute file path

--- RunFolder/RunBot.bat
+++ RunFolder/RunBot.bat
@@ -5,8 +5,8 @@ SET "CURRENT_DIR=%~dp0"
 
 REM Move to main bot folder
 cd /d "%CURRENT_DIR%\.."
-cd /d "%CD%\..\ChinoKafu\ChinoKafuu\bin\Debug\net7.0
-call ChinoBot.exe
+cd /d "%CD%\..\ChinoKafu\ChinoKafuu
+dotnet run
 
 ECHO Run successfully
 PAUSE
